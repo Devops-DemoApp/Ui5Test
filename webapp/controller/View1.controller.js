5fee64d7cf7793ae74f769a4a634589ec3096cd2
Devops-DemoApp <kamini.patil@capgemini.com>
2020-02-12 15:02:45 +0000

    fixed

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,12 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("com.dev.DemoProjectFiori.controller.View1", {
 		onInit: function () {
-  var foo= "Test Eslint";
-		},
-		function addOne(i) {
-    |          ^
-  2 |     if (i != NaN) {
-  3 |         return i ++
-  4 |     } else {   
+  var foo=== 10;
+		}
 	});
 });

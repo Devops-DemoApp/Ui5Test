3a7a1317652b8a571ea20cf1e44b71610507f9aa
Devops-DemoApp <kamini.patil@capgemini.com>
2020-03-03 12:45:20 +0000

    commit

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,30 @@
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
 
-    frameworks: ["ui5"],
+	config.set({
 
-    ui5: {
-      url: "https://openui5.hana.ondemand.com"
-    },
+		logLevel: config.LOG_INFO,
 
-    browsers: ["Chrome"]
+		browserConsoleLogOptions: {
+			level: 'info'
+		},
 
-  });
+		frameworks: ['ui5'],
+
+		ui5: {
+			url: 'http://localhost:5000',
+			mode: 'html',
+			testpage: 'webapp/test/integration/opaTests.qunit.html'
+		},
+
+		browsers: ['ChromeHeadless'],
+
+		concurrency: 1,
+
+		singleRun: true,
+
+		browserNoActivityTimeout: 600000,
+
+		proxyValidateSSL: false
+
+	});
 };
